12f2b4d3b7f3152ad2a8a234405beefac4ca8fd2
ГерценЕ <@227_TEACHER>
2018-04-03 21:54:48 +0600

    Add project files.

---
+++ ConsoleApplication9/Function.cpp
@@ -0,0 +1,292 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+#include <math.h>
+#include "Header.h"
+int * GetMemory(int *length)
+{
+	int *mas = NULL;
+	printf("Введите длину массива: ");
+	scanf("%d", length);
+	mas = (int*)calloc(*length, sizeof(int));
+	if (mas == NULL)
+	{
+		printf("Not enough memory.\n");
+		exit(0);
+	}
+	else
+		return mas;
+}
+void fillArray(int *mass, int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		*(mass + i) = 10 + rand() % 90;
+	}
+}
+void printArray(int *mass, int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		printf("[%d]", *(mass + i));
+	}
+	printf("\n");
+}
+void fillMatrix(int *mass, int length, int row)
+{
+	for (short int i = 0; i < length; i++)
+		for (int j = 0; j < row; j++)
+		{
+			*(mass + i * length + j) = -40 + rand() % 90;
+		}
+}
+void printMatrix(int *mass, int length, int row)
+{
+	for (short int i = 0; i < length; i++)
+	{
+		for (int j = 0; j < row; j++)
+		{
+			printf("%d\t", *(mass + i*length + j));
+		}
+		printf("\n");
+	}
+}
+void SearchMax(int *mass, int *mass1, int length, int row)
+{
+	mass1 = (int*)malloc(sizeof(int)*length);
+	int max = 0;
+	int i = 0;
+	for (i = 0; i < length; i++)
+	{
+		max = 0;
+		for (int j = 0; j < row; j++)
+		{
+			if (*(mass + i*length + j) > max)
+			{
+				max = *(mass + i*length + j);
+			}
+		}
+		*(mass1 + i) = max;
+		//mass1 = (int*)realloc(mass1, i+1);*/
+	}
+	//mass1 = (int*)realloc(mass1, i - 1);
+	for (i = 0; i < 4; i++)
+	{
+		printf("%d\t", *(mass1 + i));
+	}
+	printf("\n");
+	SearchMin(mass1, length);
+}
+void SearchMin(int *mass1, int length)
+{
+	int min = *mass1;
+	for (int i = 0; i < length; i++)
+	{
+		if (*(mass1 + i) < min)
+			min = *(mass1 + i);
+	}
+	printf("%d\n", min);
+}
+void ReversNumber(int *mass, int *mass1, int length)
+{
+	int a, b;
+	for (int i = 0; i < length; i++)
+	{
+		a = *(mass + i) / 10;
+		b = *(mass + i) % 10;
+		*(mass1 + i) = b * 10 + a;
+	}
+}
+void ArifmProgres(int *mass1, int length)
+{
+	int d;
+	for (short int i = 0; i < length; i++)
+	{
+		if (*(mass1 + i) == (*mass1 + i * (*(mass1 + i) - *(mass1 + i - 1))))
+		{
+			d = *(mass1 + i) - *(mass1 + i - 1);
+			printf("Числа образует арифм. прогрессию, её разность = %d\n", d);
+		}
+		else
+			printf("Not образует. \n");
+	}
+
+}
+int SearchSecondPositivElement(int *mass, int length)
+{
+	int counter = 0;
+	for (short int i = 0; i < length; i++)
+	{
+		if (*(mass + i) > 0)
+		{
+			counter++;
+			if (counter == 2)
+			{
+				return i;
+			}
+		}
+	}
+}
+void ReverseElements(int *mas, int length)
+{
+	int check = length - 1, m;
+	for (short int i = 0; i < length / 2; i++)
+	{
+		m = *(mas + i);
+		*(mas + i) = *(mas + check);
+		*(mas + check) = m;
+		check--;
+	}
+}
+void ElementNeKrat3(int *mass, int length)
+{
+	double summa = 0, sr = 0;
+	for (short int i = 0; i < length; i++)
+		if (*(mass + i) % 3 != 0)
+		{
+			summa += *(mass + i);
+			sr++;
+		}
+	printf("Сумма элементов, не кратных 3 = %lf\n", summa);
+	printf("Среднее арифметическое элеметов, не кратных 3 = %lf\n", summa / sr);
+}
+void SwitchMinAndLastElement(int *mass, int length)
+{
+	int min = *mass, s = 0, m;
+	for (short int i = 0; i < length; i++)
+	{
+		if (min > *(mass + i))
+		{
+			s = i;
+			min = *(mass + i);
+		}
+	}
+	printf("\n");
+	printf("Минимальный элемент массива = %d\n", min);
+	m = *(mass + length - 1);
+	*(mass + length - 1) = min;
+	*(mass + s) = m;
+}
+long int TwoTo8(int *mass)
+{
+	long int proizv = 1;
+	for (short int i = 2; i <= 8; i++)
+	{
+		proizv *= *(mass + i);
+	}
+	return proizv;
+}
+void ChetOrNechet(int *mas, int length)
+{
+	int counter1 = 0, counter2 = 0;
+	printf("Массив, созданный из чётных элементов предыдущего: \n");
+	for (short int i = 0; i < length; i++)
+	{
+		if (*(mas + i) % 2 == 0)
+		{
+			counter1++;
+			printf("%d\t", *(mas + i));
+		}
+	}
+	printf("\n");
+	printf("Массив, созданный из нечётных элементов предыдущего: \n");
+	for (short int i = 0; i < length; i++)
+	{
+		if (*(mas + i) % 2 != 0)
+		{
+			counter2++;
+			printf("%d\t", *(mas + i));
+		}
+	}
+	printf("\n");
+	printf("Чётных элементов %d.\n", counter1);
+	printf("Нечётных элементов %d.\n", counter2);
+	if (counter1 < counter2)
+		printf("Чётных элементов меньше, чем нечётных.\n");
+	else if (counter1 > counter2)
+		printf("Чётных элементов больше, чем нечётных.\n");
+	else if (counter1 == counter2)
+		printf("Одинаковое количество чётных и нечётных элементов.\n");
+}
+void NotBiggerThan10(int *mas, int length)
+{
+	int counter = 0;
+	for (short int i = 0; i < length; i++)
+		if (*(mas + i) > 0 && *(mas + i) > 10)
+			counter++;
+
+	int *MASSSS = (int*)calloc(counter, sizeof(int));
+	int k = 0;
+	for (short int i = 0; i < length; i++)
+	{
+		if (*(mas + i) > 0 && *(mas + i) > 10)
+		{
+
+			*(MASSSS + k) = *(mas + i);
+			k++;
+		}
+	}
+	//printArray(MASSSS, counter);
+	printf("Кол-во положительных элементов = %d\n", counter);
+	free(MASSSS);
+}
+void Neighbor(int *mass, int length)
+{
+	int num, min = *mass, index = 0;
+	printf("Введите число: ");
+	scanf("%d", &num);
+	for (short int i = 0; i < length; i++)
+	{
+		if (num < 0)
+		{
+			if (abs(*(mass + i) - num) <= min)
+			{
+				min = abs(*(mass + i) - num);
+				index = i;
+			}
+		}
+		else
+			if ((*(mass + i) - num) <= min)
+			{
+				min = (*(mass + i) - num);
+				index = i;
+			}
+	}
+	printf("Значение = %d; Индекс = %d\n", min, index);
+}
+void SortOpredStolbca(int *mass, int column, int row)
+{
+	int ROW, temp = 0, length = column;
+	printf("Введите столбец массива, который хотите отсортировать: ");
+	scanf("%d", &ROW);
+	for (int i = 0; i < column; i++)
+	{
+		for (int j = row - 1; j > i; j--)
+		{
+			int k = j - 1;
+			if (*(mass + j * column + ROW) > *(mass + k * column + ROW))
+			{
+				temp = *(mass + j * column + ROW);
+				*(mass + j * column + ROW) = *(mass + k * column + ROW);
+				*(mass + k * column + ROW) = temp;
+			}
+		}
+	}
+	printMatrix(mass, length, row);
+}
+void Pure(int *mass, int column, int row)
+{
+	int sum = 0;
+	for (int i = 0; i < column; i++)
+	{
+		for (int j = 0; j < row; j++)
+		{
+			if (*(mass + i * column + j) % 2 != 0 && *(mass + i * column + j) < 0)
+			{
+				sum += abs(*(mass + i * column + j));
+			}
+		}
+	}
+	printf("Сумма по модулю отриц., нечётных элементов = %d\n", sum);
+}
---
+++ ConsoleApplication9/Main.cpp
@@ -0,0 +1,126 @@
+﻿#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "Header.h"
+void main()
+{
+	setlocale(LC_ALL, "rus");
+	srand(time(NULL));
+	short int w;
+	printf("Введите номер задания: ");
+	scanf("%hu", &w);
+	switch (w)
+	{
+	case 1:
+	{
+		/*2.	В прямоугольной матрице найти наибольший из
+			минимальных элементов строк матрицы*/
+		int *mass = NULL;
+		int *mass1 = NULL;
+
+		mass = (int*)malloc(sizeof(int) * 4 * 5);
+		fillMatrix(mass, 4, 5);
+		printMatrix(mass, 4, 5);
+		SearchMax(mass, mass1, 4, 5);
+		free(mass);
+	}
+	break;
+	case 2:
+	{
+		/*3.	Ввести массив, состоящий из 15 - ти элементов(двузначные целые числа).
+			Изменить разрядность цифр, образующих элементы исходного массива и, таким образом,
+			сформировать новый массив.Например, исходный массив : 25 71 84…,
+			новый массив : 52 17 48….*/
+		int *mass = NULL;
+		mass = (int*)malloc(sizeof(int) * 15);
+		int *mass1 = NULL;
+		mass1 = (int*)malloc(sizeof(int) * 15);
+		fillArray(mass, 15);
+		printArray(mass, 15);
+		ReversNumber(mass, mass1, 15);
+		printArray(mass1, 15);
+		free(mass);
+		free(mass1);
+	}
+	break;
+	case 3:
+	{
+		/*6.	Задан целочисленный массив.Вывести индексы тех элементов,
+			значения которых больше, чем у стоящих справа от него.
+			Определить количество таких чисел*/
+		int *mass = NULL;
+		int *index = NULL;
+		index = (int*)calloc(1, sizeof(int));
+		mass = (int*)malloc(sizeof(int) * 15);
+		fillArray(mass, 15);
+		printArray(mass, 15);
+		int count = 0;
+		for (int i = 0; i < 14; i++)
+		{
+			if (*(mass + i) > *(mass + i + 1))
+			{
+				*(index + count) = i;
+				count++;
+				index = (int*)realloc(index, sizeof(int)*(count + 1));
+			}
+		}
+		for (int i = 0; i < count; i++)
+		{
+			printf("%d\t", *(index + i));
+		}
+		printf("\n");
+		free(index);
+		free(mass);
+
+	}
+	break;
+	case 7:
+	{
+		//7.Задан целочисленный массив. Определить, образуют ли значения его элементов 
+		//арифметическую прогрессию. Если «да» – вывести разность прогрессии, 
+		//если «нет» – ответ «не образуют».
+		int *mass = NULL, length = 10;
+		mass = (int*)malloc(sizeof(int) * 10);
+		fillArray(mass, length);
+		printArray(mass, length);
+		int *mass1 = NULL;
+		mass1 = (int*)malloc(sizeof(int) * 10);
+		for (short int i = 0; i < length; i++)
+		{
+			*(mass1 + i) = *(mass + i);
+		}
+		for (short int i = 0; i < length; i++)
+			printf("%d\t", *(mass1 + i));
+		printf("\n");
+		ArifmProgres(mass1, length);
+		free(mass);
+		free(mass1);
+	}
+	break;
+	case 8:
+	{
+		//8.Дан массив вещественных чисел. Определить элемент массива(значение и индекс), 
+		//который наиболее близок к заданному вещественному числу R.
+		int *mass = NULL, length = 10;
+		mass = (int*)malloc(sizeof(int) * 10);
+		fillArray(mass, length);
+		printArray(mass, length);
+		Neighbor(mass, length);
+		free(mass);
+	}
+	break;
+	case 10:
+	{
+		//10.Дан двухмерный массив 7×7. Найти сумму модулей отрицательных нечётных элементов.
+		int *mass = NULL, length = 7, row = 7;
+		mass = (int*)malloc(sizeof(int) * 49);
+		fillMatrix(mass, length, row);
+		printMatrix(mass, length, row);
+		Pure(mass, length, row);
+		SortOpredStolbca(mass, length, row);
+		free(mass);
+	}
+	}
+}
